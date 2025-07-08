cc903d84b52ed99594e23b69612457a66d0b82c3
eNoodles <eNoodles@users.noreply.github.com>
2025-07-08 14:07:17 -0700

    updated testscript

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+    private Transform myTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myTransform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
